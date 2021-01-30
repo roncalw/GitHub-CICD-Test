a1f1a6c9eabf9325feb24c969a2c3aa35d69b0b1
Carlo Roncallo <roncalw@hotmail.com>
2021-01-30 14:32:55 -0500

    Added more text to the welcome mesage!

--- cfapp99/Startup.cs
+++ cfapp99/Startup.cs
@@ -32,7 +32,7 @@ namespace cfapp99
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Welcome to CodeFest!");
+                    await context.Response.WriteAsync("Welcome to CodeFest! Check out the latest POCs!");
                 });
             });
         }
